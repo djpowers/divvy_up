b6ceee84f964add6e21bfc724cdcb00d90f93545
Dave Powers <djpowers89@gmail.com>
2015-03-01 21:53:46 -0500

    Allow creation of list items

--- lib/divvy_up/list.rb
+++ lib/divvy_up/list.rb
@@ -1,4 +1,9 @@
 module DivvyUp
   class List
+    attr_reader :items
+
+    def initialize(items)
+      @items = items
+    end
   end
 end
--- spec/divvy_up/list_spec.rb
+++ spec/divvy_up/list_spec.rb
@@ -4,5 +4,10 @@ module DivvyUp
     it "exists" do
       expect(DivvyUp::List)
     end
+
+    it "creates a new list" do
+      list = DivvyUp::List.new({juice: 3, apple: 1.20, chicken: 7.99})
+      expect(list.items).to eql({juice: 3, apple: 1.20, chicken: 7.99})
+    end
   end
 end
