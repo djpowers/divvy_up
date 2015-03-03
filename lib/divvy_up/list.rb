f73da35d1cfe2808d1c525507c07fd1a2d03b09e
Dave Powers <djpowers89@gmail.com>
2015-03-02 23:06:23 -0500

    Implement split method for two-item list

--- lib/divvy_up/list.rb
+++ lib/divvy_up/list.rb
@@ -6,8 +6,10 @@ module DivvyUp
       @items = items
     end
 
-    def split(number_of_groups)
-      [self.items]
+    def split(groups)
+      return [self.items] if groups == 1
+      return [{self.items.keys[0] => self.items.values[0]},
+              {self.items.keys[1] => self.items.values[1]}]
     end
   end
 end
