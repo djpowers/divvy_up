aa15b4498b2741ee79be4cf31852fc42fd880841
Dave Powers <djpowers89@gmail.com>
2015-03-06 23:18:00 -0500

    Working implementation for splitting small list into two groups

--- lib/divvy_up/list.rb
+++ lib/divvy_up/list.rb
@@ -8,10 +8,78 @@ module DivvyUp
 
     def split(groups)
       return [self.items] if groups == 1
-      return [
-              {self.items.keys[0] => self.items.values[0]},
-              {self.items.keys[1] => self.items.values[1]}
-             ]
+      permutations = generate_list_permutations
+      permutation_price_differences = calculate_permutation_price_differences(permutations)
+      sorted_price_differences = generate_list_combinations(permutation_price_differences)
+      list_possibilities = find_full_list(permutation_price_differences, sorted_price_differences)
+      output_final_lists(list_possibilities)
+    end
+
+    private
+
+    def target_amount(divisor)
+      (self.items.values.reduce(:+) / divisor).round(2)
+    end
+
+    def generate_list_permutations
+      permutations = []
+      self.items.keys.size.times do |n|
+        sets = self.items.keys.combination(n+1)
+        sets.each do |set|
+          permutations << set
+        end
+      end
+      permutations
+    end
+
+    def calculate_permutation_price_differences(permutations)
+      permutation_price_differences = {}
+      permutations.each do |permutation|
+        total = 0.0
+        permutation.each_with_index do |item, n|
+          total += self.items[item]
+        end
+        permutation_price_differences[permutation] = (target_amount(2) - total).abs
+      end
+      permutation_price_differences
+    end
+
+    def generate_list_combinations(permutation_price_differences)
+      permutation_price_differences.values.sort
+    end
+
+    def find_full_list(permutation_price_differences, sorted_price_differences)
+      list = []
+      sorted_price_differences.each do |difference|
+        if sorted_price_differences.count(difference) == 1
+          list << permutation_price_differences.key(difference)
+        else
+          permutation_price_differences.find_all{|k,v| v == difference}.map(&:first).each do |permutation|
+            list << permutation
+          end
+          sorted_price_differences.delete(difference)
+        end
+      end
+      flattened_list = list.flatten
+      if self.items.keys.sort == flattened_list.uniq.sort
+        output = []
+        list.each do |sublist|
+          item_price = {}
+          sublist.each do |item|
+            item_price[item] = self.items[item]
+          end
+          output << item_price
+        end
+      end
+      output
+    end
+
+    def output_final_lists(lists)
+      index = 1
+      until (lists.first.keys + lists[index].keys).sort == self.items.keys.sort
+        index += 1
+      end
+      [lists.first, lists[index]]
     end
   end
 end
